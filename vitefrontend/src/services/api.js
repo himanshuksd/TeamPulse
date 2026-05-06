9028e4ce16d2a1bd58375ed13beb046d099971b2
kasaudhanshivani <kasaudhanshivani027@gmail.com>
2026-05-06 13:43:45 +0530

    Fix production API URL

--- vitefrontend/src/services/api.js
+++ vitefrontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_API_URL || "https://himanshuksd-teampulse.onrender.com";
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://himanshuksd-teampulse.onrender.com";
+
 const api = axios.create({
   baseURL: API_URL,
 });
@@ -9,9 +11,11 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
